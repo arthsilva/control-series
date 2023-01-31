bf86b20f485f651f709fdd962d7d9930d729220b
arthsilva <arthurss.93@gmail.com>
2023-01-31 18:36:10 -0300

    Iniciado teste simples com view e php utilizando componente com o blade

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        $series = [
+            'Punisher',
+            'Lost',
+            'Grey\'s Anatomy'
+        ];
+
+        return view('series.index', compact('series'));
+    }
+}
