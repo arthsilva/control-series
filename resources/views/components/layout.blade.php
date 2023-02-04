269707487a7f4b8109e1c6ba4f8650e33d0ea588
arthsilva <arthurss.93@gmail.com>
2023-02-04 16:32:07 -0300

    Instalação do bootstrap utilizando o Laravel-mix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>{{ $title }} - Controle de Séries</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         <h1>{{ $title }}</h1>
